6fadd5aed0572a508aef90827b36a562296e1211
Filippo Taiti <129044561+fili-taiz@users.noreply.github.com>
2024-02-23 15:51:39 +0100

    Rimossi file da non tracciare

--- cmake-build-debug/googletest-main/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ale/Scrivania/Progetto/googletest-main/googletest
-# Build directory: /home/ale/Scrivania/Progetto/cmake-build-debug/googletest-main/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
